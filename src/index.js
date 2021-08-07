72736c9c08dd7948fc709c22e2ab47b9ed15fdc7
Pavel Martynov <p.martynov@antraks.ru>
2021-08-07 13:51:58 +0300

    В список городов попадают значения только при удачном результате.

--- src/index.js
+++ src/index.js
@@ -11,36 +11,30 @@ import {
 
 // eslint-disable-next-line func-names
 (async function () {
-  // Получаем указатели на нужные элементы
   const weatherForm = document.querySelector("form");
   const weatherInfoEl = document.querySelector("#weatherInfo");
 
   await showWeatherLocation(weatherInfoEl);
 
-  // Список 10 последних городов
   const listOfTowns = await loadListOfTowns();
   drawList(document.querySelector("#towns"), listOfTowns);
 
   weatherForm.addEventListener("submit", async (ev) => {
-    // чтобы не перезагружать страницу
     ev.preventDefault();
 
-    // читаем значение из формы
     const formElement = ev.target;
     const inputEl = formElement.querySelector("input");
     const cityName = inputEl.value;
     inputEl.value = "";
 
-    // Показ погоды
     const weather = await getWeather(cityName);
     showWeather(weatherInfoEl, weather);
     if (weather.cod === 200) {
       const { lon, lat } = weather.coord;
       updateMap(lat, lon);
+      addTownInList(listOfTowns, cityName);
+      await saveListOfTowns(listOfTowns);
+      drawList(document.querySelector("#towns"), listOfTowns);
     }
-    // Сохранение города в список
-    addTownInList(listOfTowns, cityName);
-    await saveListOfTowns(listOfTowns);
-    drawList(document.querySelector("#towns"), listOfTowns);
   });
 })();
